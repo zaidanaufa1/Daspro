b5af7af4302c66cac41704f06b005ffe37518149
gaswicak <52844928+gaswicak@users.noreply.github.com>
2019-11-22 20:13:18 +0700

    Update main.pas

--- Pascal/main.pas
+++ Pascal/main.pas
@@ -1,6 +1,6 @@
 Program main;
 
-uses PokebonCSV,F01,F02,F03,F04,F06,F07,F08,F09,F10,F11,F12,F13,F14,F15;
+uses PokebonCSV,F01,F02,F03,F04,F05,F06,F07,F08,F09,F10,F11,F12,F13,F14,F15;
 
 var
     pok,evo,trn:string;
