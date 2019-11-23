def710624e2c062a227f31257f598c5ceee27a77
Danendra A.H.P <danendra52@gmail.com>
2019-11-23 16:24:51 +0700

    c

--- Pascal/F06.pas
+++ Pascal/F06.pas
@@ -2,22 +2,22 @@ unit F06;
 
 interface
     uses 
-        PokebonCSV,sysutils,F03;
+        PokebonCSV,sysutils,F03,F05;
     procedure tangkap();
 
 implementation
     procedure tangkap();
     var
         i : integer;
-        enemy : string;
+        new : string;
     begin
         //writeln('sebelum',jmlInv);
-        //enemy := EncounterChance(Tencounter, Tencounter.Neff);
-        writeln('Kamu mendapatkan: pikaboo');
+        new := EncounterChance(Tencounter, Tencounter.Neff);
+        writeln('Kamu mendapatkan: ',new);
         writeln('Isi inventori saat ini:');
         jmlInv := jmlInv+1;
         TInv[jmlInv-1].Nomor_Inventori := IntToStr(jmlInv-1);
-        TInv[jmlInv-1].Nama_Pokebon := 'pikaboo';
+        TInv[jmlInv-1].Nama_Pokebon := new;
         TInv[jmlInv-1].Level := '1';
         TInv[jmlInv-1].Kondisi := 'lelah';
         listInven();
